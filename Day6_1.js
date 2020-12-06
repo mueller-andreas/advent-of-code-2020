19fc04c9e2f3da960d0668f3ef3300349d8f5627
Andreas Müller <mueller.andreas25@gmail.com>
2020-12-06 19:58:00 +0100

    Day 6 part 1

--- Day6_1.js
+++ Day6_1.js
@@ -1,8 +1,23 @@
 const fs = require("fs");
 
-function treeCount(filename) {
-    const data = fs.readFileSync(filename, 'utf8').split("\n");
-
+function answerCount(filename) {
+    const groups = fs.readFileSync(filename, 'utf8').split("\n\n");
+    //const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let sum = 0;
+    groups.forEach(group => {
+        //console.log(group);
+        let allAnswers = group.split("\n");
+        //console.log(allAnswers);
+        let uniqAnswers = new Set();
+        allAnswers.forEach(answer => {
+            for (const letter of answer) {
+                uniqAnswers.add(letter);
+            }
+        });
+        sum += uniqAnswers.size;
+        //console.log(uniqAnswers.size);
+    });
+    return sum;
 }
 
-console.log(treeCount('./Input/Day6.txt'));
+console.log(answerCount('./Input/Day6.txt'));
