b8ca21078c8ab79f7d8fb690cd4b83f117dad4f0
Andreas Müller <mueller.andreas25@gmail.com>
2020-12-10 01:40:02 +0100

    Day 9 part 2

---
+++ Day9_2.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const { get } = require("http");
+
+function getRange(filename) {
+    const numbers = fs.readFileSync(filename, 'utf8').split("\n");
+    let lowerEnd = 0;
+    let upperEnd = 1;
+    let sum = 0;
+    const solution = 15353384;
+    //const solution = 127;
+    sum = parseInt(numbers[lowerEnd]) + parseInt(numbers[upperEnd]);
+    while (sum != solution) {
+        if (sum < solution) {
+            upperEnd++;
+            sum += parseInt(numbers[upperEnd]);
+        } else {
+            lowerEnd++;
+            upperEnd = lowerEnd + 1;
+            sum = parseInt(numbers[lowerEnd]) + parseInt(numbers[upperEnd]);
+        }
+    }
+    return getLowAndHigh(numbers, [lowerEnd, upperEnd]);
+}
+
+function getLowAndHigh(numbers, rangeInfo) {
+    let low = Number.MAX_SAFE_INTEGER;
+    let high = 0;
+
+    for(let i = rangeInfo[0]; i < rangeInfo[1]; i++) {
+        if (parseInt(numbers[i]) < low) {
+            low = parseInt(numbers[i]);
+        }
+        if (parseInt(numbers[i]) > high) {
+            high = parseInt(numbers[i]);
+        }
+    }
+    return low + high;
+}
+
+console.log(getRange('./Input/Day9.txt'));
+//console.log(encodingCheck('./Input/test.txt'));
